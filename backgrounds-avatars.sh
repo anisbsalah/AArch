67522b28b9a3a1b3cd794d93b007c5b937786cdb
anisbsalah <anisbensalah.89@gmail.com>
2024-02-01 03:07:50 +0100

    backgrounds-avatars.sh

---
+++ backgrounds-avatars.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author  :  anisbsalah
+# Github  :  https://github.com/anisbsalah
+##################################################################################################################
+#
+# DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# CURRENT_DIR="$(pwd)"
+##################################################################################################################
+
+echo
+tput setaf 3
+echo "######################################################################################################"
+echo "################# Installing personal backgrounds and avatars..."
+echo "######################################################################################################"
+tput sgr0
+echo
+
+[[ -d /usr/share/backgrounds/AbS-Wallpapers ]] || sudo mkdir -p /usr/share/backgrounds/AbS-Wallpapers
+sudo cp -v "${CURRENT_DIR}/Personal/settings/backgrounds/desktop_bg.jpg" /usr/share/backgrounds/AbS-Wallpapers/
+sudo cp -v "${CURRENT_DIR}/Personal/settings/backgrounds/login_bg1.jpg" /usr/share/backgrounds/AbS-Wallpapers/
+sudo cp -v "${CURRENT_DIR}/Personal/settings/backgrounds/login_bg2.jpg" /usr/share/backgrounds/AbS-Wallpapers/
+sudo cp -v "${CURRENT_DIR}/Personal/settings/.face" /usr/share/backgrounds/AbS-Wallpapers/avatar.jpg
+cp -v "${CURRENT_DIR}/Personal/settings/.face" "${HOME}/.face"
+
+echo
+tput setaf 2
+echo "######################################################################################################"
+echo "################# Images installed"
+echo "######################################################################################################"
+tput sgr0
+echo
