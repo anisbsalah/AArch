7999ccdb92939e0b94c2edef103368439e787d1a
anisbsalah <anisbensalah.89@gmail.com>
2024-05-26 06:37:29 +0100

    sudo

--- microcode.sh
+++ microcode.sh
@@ -22,10 +22,10 @@ echo
 PROC_TYPE=$(lscpu)
 if grep -Eiq "GenuineIntel" <<<"${PROC_TYPE}"; then
 	printf "[*] Installing Intel microcode...\n\n"
-	pacman -S --noconfirm --needed intel-ucode
+	sudo pacman -S --noconfirm --needed intel-ucode
 elif grep -Eiq "AuthenticAMD" <<<"${PROC_TYPE}"; then
 	printf "[*] Installing AMD microcode...\n\n"
-	pacman -S --noconfirm --needed amd-ucode
+	sudo pacman -S --noconfirm --needed amd-ucode
 fi
 
 SEP="########################################"
@@ -35,9 +35,7 @@ echo "########################################"
 echo "[*] Adding microcode to HOOKS..."
 echo "########################################"
 
-grep '^HOOKS=' /etc/mkinitcpio.conf | if grep -q "microcode"; then echo "    microcode already in." && echo "${SEP}"; else sed -i '/^HOOKS=/s/autodetect\( \|$\)/autodetect microcode\1/g' /etc/mkinitcpio.conf; fi
-
-sudo mkinitcpio -P
+grep '^HOOKS=' /etc/mkinitcpio.conf | if grep -q "microcode"; then echo "    microcode already in." && echo "${SEP}"; else sudo sed -i '/^HOOKS=/s/autodetect\( \|$\)/autodetect microcode\1/g' /etc/mkinitcpio.conf && sudo mkinitcpio -P; fi
 
 echo
 tput setaf 2
