4902172d7cff18009d7c8c9cd398413921ccc3aa
anisbsalah <anisbensalah.89@gmail.com>
2024-05-26 05:10:09 +0100

    upd get repos scripts

--- repos/get-chaotic-aur-repo.sh
+++ repos/get-chaotic-aur-repo.sh
@@ -11,6 +11,35 @@
 # CURRENT_DIR="$(pwd)"
 ##################################################################################################################
 
+echo
+tput setaf 4
+echo "######################################################################################################"
+echo "> Installing the keyring and mirrorlist for Chaotic-AUR"
+echo "######################################################################################################"
+tput sgr0
+echo
+
+tput setaf 3
+echo "[*] Getting the primary key for Chaotic-AUR..."
+tput sgr0
+echo
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+echo
+tput setaf 3
+echo "[*] Getting the chaotic keyring..."
+tput sgr0
+echo
+sudo pacman -U --noconfirm --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+
+echo
+tput setaf 3
+echo "[*] Getting the chaotic mirrorlist..."
+tput sgr0
+echo
+sudo pacman -U --noconfirm --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 if grep -q chaotic /etc/pacman.conf; then
 
 	echo
@@ -24,35 +53,6 @@ if grep -q chaotic /etc/pacman.conf; then
 	exit 0
 
 else
-
-	echo
-	tput setaf 4
-	echo "######################################################################################################"
-	echo "> Installing the keyring and mirrorlist for Chaotic-AUR"
-	echo "######################################################################################################"
-	tput sgr0
-	echo
-
-	tput setaf 3
-	echo "[*] Getting the primary key for Chaotic-AUR..."
-	tput sgr0
-	echo
-	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-	sudo pacman-key --lsign-key 3056513887B78AEB
-
-	echo
-	tput setaf 3
-	echo "[*] Getting the chaotic keyring..."
-	tput sgr0
-	echo
-	sudo pacman -U --noconfirm --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-
-	echo
-	tput setaf 3
-	echo "[*] Getting the chaotic mirrorlist..."
-	tput sgr0
-	echo
-	sudo pacman -U --noconfirm --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 	echo
 	tput setaf 3
