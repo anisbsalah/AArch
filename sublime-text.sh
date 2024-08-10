368b2e7f089bab030cca9b371343c831ff6ac1ca
anisbsalah <anisbensalah.89@gmail.com>
2024-08-10 15:58:08 +0100

    download pkgbuild & install

--- sublime-text.sh
+++ sublime-text.sh
@@ -19,11 +19,8 @@ echo "##########################################################################
 tput sgr0
 echo
 
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-
-sudo pacman -Syu --noconfirm sublime-text
+wget https://download.sublimetext.com/sublime-text-4180-1-x86_64.pkg.tar.xz -O /tmp/sublime-text-x86_64.pkg.tar.xz
+sudo pacman -U --noconfirm --needed /tmp/sublime-text-x86_64.pkg.tar.xz
 
 echo
 tput setaf 2
